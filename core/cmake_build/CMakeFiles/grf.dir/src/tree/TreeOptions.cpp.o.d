35cdb3ef31c882240d8bb9071fc1c2b15cf1b573
Sermet Pekin <sermet.pekin@gmail.com>
2025-10-11 13:15:09 +0300

    tests

---
+++ core/cmake_build/CMakeFiles/grf.dir/src/tree/TreeOptions.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/grf.dir/src/tree/TreeOptions.cpp.o: \
+  /Users/sermetpekin/Desktop/git_repos/grf/core/src/tree/TreeOptions.cpp \
+  /Users/sermetpekin/Desktop/git_repos/grf/core/src/tree/TreeOptions.h \
+  /Users/sermetpekin/Desktop/git_repos/grf/core/src/commons/globals.h
